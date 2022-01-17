f02f18db1fb43503b3e219c28f16b510bc055fd6
dwikydinan <58415713+dwikydinan@users.noreply.github.com>
2022-01-17 13:55:26 +0700

    tescommerce

---
+++ lib/src/pages/loginpage.dart
@@ -0,0 +1,225 @@
+import 'dart:html';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_ecommerce_app/src/pages/home_page.dart';
+import 'package:flutter_ecommerce_app/src/themes/light_color.dart';
+import 'package:flutter_ecommerce_app/src/themes/theme.dart';
+import 'package:flutter_ecommerce_app/src/widgets/BottomNavigationBar/bottom_navigation_bar.dart';
+import 'package:flutter_ecommerce_app/src/widgets/title_text.dart';
+import 'package:flutter_ecommerce_app/src/widgets/extentions.dart';
+import 'package:http/http.dart' as http;
+
+class loginpages extends StatefulWidget {
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+  
+  Widget buildEmail() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('Email',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.bold
+        ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 6,
+                offset: Offset(0,2)
+              )
+            ]
+          ),
+          height: 60,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.black87
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14),
+              prefixIcon: Icon(Icons.email,
+              color: Color(0xff2890c8)
+              ),
+              hintText: 'Email',
+              hintStyle: TextStyle(
+                color: Colors.black38
+                
+              )
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+Widget buildPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('Password',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.bold
+        ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 6,
+                offset: Offset(0,2)
+              )
+            ]
+          ),
+          height: 60,
+          child: TextField(
+           obscureText: true,
+            style: TextStyle(
+              color: Colors.black87
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14),
+              prefixIcon: Icon(
+                Icons.lock,
+              color: Color(0xff2890c8)
+              ),
+              hintText: 'Password',
+              hintStyle: TextStyle(
+                color: Colors.black38
+                
+              )
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+Widget buildForgotPassBtn(){
+  return Container(
+    alignment: Alignment.centerRight,
+    child: FlatButton(
+      onPressed: () => print("Forgot Password pressed"),
+      padding: EdgeInsets.only(right: 0),
+      child: Text(
+        'Forgot Password?',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold ),
+      ),
+    ),
+  );
+}
+
+Widget buildRememberCb(){
+  return Container(
+    height: 20,
+    child: Row(
+    child: <Widget>[
+      Theme(data: ThemeData(unselectedWidgetColor: Colors.white), 
+      child: Checkbox(value: isRememberMe,
+      checkColor: Colors.blue,
+      activeColor: Colors.white,
+      onChanged: (value) {
+        setState(() { isRememberMe = value;
+        });
+      },
+      )
+      ),
+      Text('Remember Me', style: TextStyle(color: Colors.white,
+      fontWeight: FontWeight.bold
+      ),
+      )
+    ],
+    ),
+  );
+}
+
+Widget buildLoginBtn(){
+  return Container(
+    padding: EdgeInsets.symmetric(vertical: 25),
+    width: double.infinity,
+    child: ElevatedButton(onPressed: () => MyHomePage(), child: null,),
+  );
+}
+
+class _LoginPageState extends State<loginpages> {
+  
+bool isRememberMe = false;
+
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(children: <Widget> [
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xff2890c8),
+                    Color(0xff5c3dff),
+                  ]
+                  )
+              ),
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              padding: EdgeInsets.symmetric(
+              horizontal: 25, 
+              vertical: 120
+              ),
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Sign In',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold
+                ),
+                ),
+              SizedBox(height: 50),
+              buildEmail(),
+              SizedBox(height: 20),
+              buildPassword(),
+              buildForgotPassBtn(),
+              buildRememberCb(),
+              buildLoginBtn()
+              ],
+            ),
+            ),
+            )
+          ],
+          ),
+        ),
+        ),
+    );
+  }
+}
---
+++ lib/src/pages/registerpage.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/src/themes/light_color.dart';
+import 'package:flutter_ecommerce_app/src/themes/theme.dart';
+import 'package:flutter_ecommerce_app/src/widgets/BottomNavigationBar/bottom_navigation_bar.dart';
+import 'package:flutter_ecommerce_app/src/widgets/title_text.dart';
+import 'package:flutter_ecommerce_app/src/widgets/extentions.dart';
